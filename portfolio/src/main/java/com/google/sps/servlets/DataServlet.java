17a4c06b06b36003a110e75781cd33f711d6866a
kibazohb <kibazohb@mcmaster.ca>
2020-02-28 00:43:39 -0800

    Week2- Update index file with button, add name-container element to style css

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,8 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String name = "Brian Kibazohi";
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Brian Kibazohi!</h1>");
+    response.getWriter().println(name);
   }
 }
