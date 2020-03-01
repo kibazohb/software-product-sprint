f656618eefcbfd85abc345ac801ea549577b2bd3
kibazohb <kibazohb@mcmaster.ca>
2020-03-01 15:28:55 -0800

    Servers -Print json data to screen on button click

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,8 +1,29 @@
 
-//this is the script js file to generate the name.
+//this is the script js file to get sample data from server
 
 function getMyName() {
   fetch('/data').then(response => response.text()).then((name) => {
     document.getElementById('name-container').innerText = name;
   });
 }
+
+/** Fetches all facts */
+
+function getMyFacts() {
+    fetch("/data").then(response => response.json()).then((facts) => {
+        console.log(facts);
+        const factsListElement = document.getElementById('liElement-container');
+        factsListElement.innerHTML = '';
+        for (let i = 0; i < facts.length; i++){
+            factsListElement.appendChild(createListElement(facts[i]))
+        }
+    });
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
+}
+
