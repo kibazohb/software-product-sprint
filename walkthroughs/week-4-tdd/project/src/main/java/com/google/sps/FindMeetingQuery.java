71371ae2f2b713e877236e5ec5e253c19d47dca0
kibazohb <kibazohb@mcmaster.ca>
2020-03-12 20:23:00 -0700

    Week-4-Algo: Sort the events by startTime

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -12,12 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* Problem 
+// you'll need to implement a feature that given the meeting information, 
+// it will return the times when the meeting could happen that day.
+*/
+
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
+  private static Collection<TimeRange> result = Collections.emptySet();
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+ 
+    if (events == null) {
+        result.add(TimeRange.WHOLE_DAY);
+    }
+    //sort the events by startTime
+    ArrayList<Event> eventList = new ArrayList<Event>(events); 
+
+    Collections.sort(eventList, Event.EVENT_START_COMPARATOR);
+
+    return result;
   }
 }
